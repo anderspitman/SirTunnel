115e1cf7ab89aec2051fdd8e05a0436d5846c0ed
devin <devin@convolinx.com>
2023-08-03 20:24:41 +0900

    implement tunnel checking and auto cleanup to address issue with stale tunnels

--- sirtunnel.py
+++ sirtunnel.py
@@ -3,7 +3,26 @@
 import sys
 import json
 import time
-from urllib import request
+from urllib import request, error
+
+
+def check_tunnel_health(tunnel_id):
+    check_url = 'http://127.0.0.1:2019/id/' + tunnel_id
+    try:
+        request.urlopen(check_url)
+    except error.HTTPError as e:
+        if e.code == 404:  # Not Found
+            return False
+    except error.URLError:
+        return False
+    return True
+
+
+def delete_tunnel(tunnel_id):
+    print("Cleaning up tunnel")
+    delete_url = 'http://127.0.0.1:2019/id/' + tunnel_id
+    req = request.Request(method='DELETE', url=delete_url)
+    request.urlopen(req)
 
 
 if __name__ == '__main__':
@@ -30,18 +49,18 @@ if __name__ == '__main__':
         'Content-Type': 'application/json'
     }
     create_url = 'http://127.0.0.1:2019/config/apps/http/servers/sirtunnel/routes'
-    req = request.Request(method='POST', url=create_url, headers=headers)
-    request.urlopen(req, body)
+    req = request.Request(method='POST', url=create_url, headers=headers, data=body)
+    request.urlopen(req)
 
     print("Tunnel created successfully")
 
     while True:
         try:
+            if not check_tunnel_health(tunnel_id):
+                print("Tunnel disconnected unexpectedly")
+                delete_tunnel(tunnel_id)
+                break
             time.sleep(1)
         except KeyboardInterrupt:
-
-            print("Cleaning up tunnel")
-            delete_url = 'http://127.0.0.1:2019/id/' + tunnel_id
-            req = request.Request(method='DELETE', url=delete_url)
-            request.urlopen(req)
+            delete_tunnel(tunnel_id)
             break
